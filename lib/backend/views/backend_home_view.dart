ee49ac2c94810c6f95fd5e824ebb14d6bc76d7fd
Taewook Kang <dongnae.kang@gmail.com>
2020-07-23 21:25:01 +0900

    no message

--- lib/backend/views/backend_home_view.dart
+++ lib/backend/views/backend_home_view.dart
@@ -1,6 +1,8 @@
 import 'package:checkin/backend/viewModels/backend_home_viewModel.dart';
 import 'package:checkin/backend/views/info_view.dart';
+import 'package:checkin/backend/views/suggestion_view.dart';
 import 'package:checkin/backend/views/visitor_view.dart';
+import 'package:checkin/frontend/views/building_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider_architecture/provider_architecture.dart';
 
@@ -11,16 +13,19 @@ class BackEndHomeView extends StatelessWidget {
       viewModelBuilder: () => BackEndHomeViewModel(),
       builder: (context, model, child) => SafeArea(
           child: DefaultTabController(
-        length: 2,
+        length: 4,
         child: Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.blue,
             leading: SizedBox(),
             flexibleSpace: SafeArea(
               child: TabBar(
                 //indicatorColor: Colors.transparent,
                 tabs: [
-                  Tab(text: "Table", icon: Icon(Icons.table_chart)),
-                  Tab(text: "Info", icon:Icon(Icons.person))
+                  Tab(text: "Table", icon: Icon(Icons.table_chart, color: Colors.white,)),
+                  Tab(text: "Sugg", icon: Icon(Icons.notification_important, color: Colors.white)),
+                  Tab(text: "Building", icon: Icon(Icons.build, color: Colors.white)),
+                  Tab(text: "Info", icon:Icon(Icons.person, color: Colors.white))
                 ],
               ),
             ),
@@ -28,6 +33,8 @@ class BackEndHomeView extends StatelessWidget {
           body: TabBarView(
             children: [
               VisitorView(),
+              SuggestionView(),
+              BuildingView(),
               InfoView(),
             ],
           ),
--- lib/backend/views/detail_view.dart
+++ lib/backend/views/detail_view.dart
@@ -1,29 +1,22 @@
-
 import 'package:checkin/models/suggestion.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DetailView extends StatelessWidget{
-
+class DetailView extends StatelessWidget {
   final Suggestion suggestion;
   DetailView({Key key, @required this.suggestion}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw 
-Container(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                children: <Widget>[
-                  Text(suggestion.title
-                  ),
-                  Text(suggestion.content
-                  ),
-                ],
-              ),
-
-            ),
-          );
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            Text(suggestion.title),
+            Text(suggestion.content),
+          ],
+        ),
+      ),
+    );
   }
-
 }
